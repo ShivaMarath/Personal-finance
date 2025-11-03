f4529b85024940d9264e2042b123552090b149f1
Shiva Marath <shivamarath2005@gmail.com>
2025-11-04 03:45:14 +0530

    landing

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,20 +6,24 @@ import Image from 'next/image'
 const Hero = () => {
   return (
     <div className='pb-20 px-4'>
-        <h1>
+        <div className='container mx-auto text-center '>
+             <h1 className='text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title'>
             Manage Your Finance <br /> with Intellegence
         </h1>
-        <p>
+        <p className='text-xl text-gray-600 mb-8 max-w-2xl mx-auto'>
             An AI powered website that helps you track and analyse your 
             spendings effortlessly with real time insights.
         </p>
-        <div>
+        
+       
+        <div className='flex justify-center space-x-4'>
             <Link href={'/'}>
                 <Button size={"lg"} className='px-8'>Get Started</Button>
             </Link>
              <Link href={'/'}>
                 <Button variant={"outline"} size={"lg"} className='px-8'>Watch Demo</Button>
             </Link>
+        </div>
         </div>
 
         <div>
