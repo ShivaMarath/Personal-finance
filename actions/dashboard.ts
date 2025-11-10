95f43b5dbf279d8d821a035642ab2a4f7abdfd9e
Shiva Marath <shivamarath2005@gmail.com>
2025-11-10 18:52:40 +0530

    dashboard action

--- actions/dashboard.ts
+++ actions/dashboard.ts
@@ -1,7 +1,36 @@
 "use server"
 import { auth } from "@clerk/nextjs/server"
 import {db} from "../lib/prisma"
-export async function createAccount(data){
+import { AccountType } from "../lib/generated/prisma"
+import { revalidatePath } from "next/cache"
+
+
+
+type DecimalLike = {
+  toNumber: () => number
+}
+type CreateAccountData = {
+  name: string
+  type: string
+  balance: string | number
+  isDefault?: boolean
+}
+
+
+type SerializedTransactionInput = {
+  balance?: DecimalLike | number
+  [key: string]: any
+}
+const serializeTransaction = (obj: SerializedTransactionInput) => {
+  const serialized = { ...obj }
+  if (obj.balance && typeof obj.balance === "object" && "toNumber" in obj.balance) {
+    serialized.balance = obj.balance.toNumber()
+  }
+  return serialized
+}
+
+
+export async function createAccount(data : CreateAccountData){
 
         try {
             const {userId} = await auth()
@@ -17,7 +46,7 @@ export async function createAccount(data){
             if(!user){
                 throw new Error("User not found")
             }
-            const floatBalance = parseFloat(data.balance)
+            const floatBalance = parseFloat( typeof data.balance === "number" ? data.balance.toString() : data.balance)
             if(isNaN(floatBalance)){
                 throw new Error ("Invalid balance amount")
             }
@@ -39,7 +68,23 @@ export async function createAccount(data){
                     }
                 })
             }
+
+            const account = await db.account.create({
+                data:{
+                    ...data,
+                    balance: floatBalance,
+                    userId:user.id,
+                    isDefault: shouldBeDefault,
+                     type: data.type as AccountType
+                }
+                
+            })
+ 
+            const serializedAccount = serializeTransaction(account)
+            revalidatePath("/dashboard") 
+            return {success:true, data: serializedAccount}
         } catch (error) {
-            
+            //@ts-ignore
+            throw new Error(error.message)
         }
 }
